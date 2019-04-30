f4b057818b413a0e645ef5bb785b27311b119a5b
sdrobny <szymondrobny@gmail.com>
2019-05-01 00:51:41 +0200

    chuje muje dzikie weze jebac stomil olsztyn

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Club;
+use App\Entity\Dictionary\Position;
+use App\Entity\Footballer;
+use App\Entity\League;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
+
+        $positions = $manager->getRepository(Position::class)->findAll();
+
+        //League
+        $league = new League();
+        $league->setCountry("Polska");
+        $league->setName("Testowa liga");
+        $manager->persist($league);
+
+        //Clubs
+        for ($c = 0; $c < 15; $c++) {
+
+            $club = new Club();
+
+            $city = $faker->city;
+            $club->setName("F.C. " . $city);
+            $club->setCity($city);
+            $club->setStadium($city . " Stadium");
+            $club->setLeague($league);
+
+            $manager->persist($club);
+
+
+            foreach ($positions as $pos) {
+
+                for ($p = 0; $p < 3; $p++) {
+
+                    $fb = new Footballer();
+                    $fb->setName($faker->firstNameMale);
+                    $fb->setSurname(ucfirst($faker->word));
+                    $fb->setBirthdate($faker->dateTime);
+                    $fb->setAssists(0);
+                    $fb->setGoals(0);
+                    $fb->setRedCards(0);
+                    $fb->setYellowCards(0);
+                    $fb->setPosition($pos);
+                    $fb->setClub($club);
+
+                    $manager->persist($fb);
+
+                }
+
+            }
+
+
+            $manager->flush();
+        }
+    }
+}
