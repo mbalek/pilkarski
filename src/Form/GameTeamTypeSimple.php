8c37a1e26638e6b38bf2d3f8c3e51a5b8b2f47b3
mbalek <nutis5555@gmail.com>
2019-05-12 17:25:49 +0200

    poprawki

--- src/Form/GameTeamTypeSimple.php
+++ src/Form/GameTeamTypeSimple.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\GameTeam;
+use App\Repository\ClubRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
--- src/Form/GameType.php
+++ src/Form/GameType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Game;
+use App\Repository\ClubRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
